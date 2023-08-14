e0f0a14fc46a00423eb54b5bff24ced787917ed7
Julia Kuroishi <109692553+juliakuroishi@users.noreply.github.com>
2023-08-14 11:10:14 -0400

    Add files via upload

---
+++ exercicios php/switch.php
@@ -0,0 +1,28 @@
+<?php
+$dia_da_semana = 1;
+
+switch ($dia_da_semana){
+    case 1:
+        echo "domingo";
+        break;
+    case 2:
+        echo "segunda";
+        break;
+    case 3:
+        echo "terça";
+        break;
+    case 4:
+        echo "quarta";
+        break;
+    case 5:
+        echo "quinta";
+        break;
+    case 6:
+        echo "sexta";
+        break;
+    case 7:
+        echo "sabado";
+        break;
+}
+
+?>
---
+++ exercicios php/while.php
@@ -0,0 +1,10 @@
+<?php
+$indice = 0;
+
+while ($indice < 20) {
+    $indice = $indice +1;
+    if ($indice = 12) {
+        echo "Numero valido";
+    }
+}
+?>
