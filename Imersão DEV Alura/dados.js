28e7ee7c9a4e2197c56ca622e62278de2026df3e
Julia Kuroishi <juliabkuroishi@gmail.com>
2024-09-04 15:59:15 -0400

    imeresao dev

---
+++ Imersão DEV Alura/dados.js
@@ -0,0 +1,65 @@
+let personagensBiblicos = [ //este é uma array com 12 objetos
+    {
+      nome: "Abraão",
+      cargo: "Patriarca",
+      "livros da bíblia": ["Gênesis"]
+    },
+    {
+      nome: "Daniel",
+      cargo: "Profeta",
+      "livros da bíblia": ["Daniel"]
+    },
+    {
+      nome: "Davi",
+      cargo: "Rei",
+      "livros da bíblia": ["I Samuel", "II Samuel", "I Reis", "Salmos"]
+    },
+    {
+      nome: "Elias",
+      cargo: "Profeta",
+      "livros da bíblia": ["I Reis", "II Reis"]
+    },
+    {
+      nome: "Ester",
+      cargo: "Rainha",
+      "livros da bíblia": ["Ester"]
+    },
+    {
+        nome: "Gideão",
+        cargo: "Juiz",
+        "livros da bíblia": ["Juízes"]
+    },      
+    {
+      nome: "Josué",
+      cargo: "Líder",
+      "livros da bíblia": ["Josué"]
+    },
+    {
+      nome: "Moisés",
+      cargo: "Profeta",
+      "livros da bíblia": ["Êxodo", "Levítico", "Números", "Deuteronômio"]
+    },
+    {
+      nome: "Mordecai",
+      cargo: "Conselheiro",
+      "livros da bíblia": ["Ester"]
+    },
+    {
+      nome: "Salomão",
+      cargo: "Rei",
+      "livros da bíblia": ["I Reis", "II Crônicas", "Provérbios", "Eclesiastes", "Cânticos"]
+    },
+    {
+      nome: "Sansão",
+      cargo: "Juiz",
+      "livros da bíblia": ["Juízes"]
+    },
+    {
+      nome: "Samuel",
+      cargo: "Profeta",
+      "livros da bíblia": ["I Samuel", "II Samuel"]
+    }
+  ];
+  
+  console.log(personagensBiblicos);
+  
