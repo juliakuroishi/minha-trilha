735a0be34b256571c2eebe6bfcd651c59473662a
Julia Kuroishi <juliabkuroishi@gmail.com>
2024-09-06 15:28:37 -0400

    up do projeto

---
+++ Imersão DEV Alura/dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        humor: "Feliz",
+        citacao: '"A verdadeira felicidade é encontrada, não na busca egoísta de prazer, mas no cumprimento do dever. Em fazer o bem aos outros, nossa própria felicidade é promovida."',
+        referencia: "— Caminho a Cristo, p. 80",
+        tags: "felicidade alegria"
+    },
+    {
+        humor: "Triste",
+        citacao: '"Os que têm suportado as maiores tristezas são frequentemente os que levam aos outros o maior conforto, introduzindo a luz do sol aonde quer que vão."',
+        referencia: "— Mensagens Escolhidas, v. 2, p. 274",
+        tags: "tristeza infeliz"
+    },
+    {
+        humor: "Raiva",
+        citacao: '"Quem vive para si está sempre pronto para se ofender. Aquele que aprende de Cristo é possuidor da paz interior. Sua mente está em repouso."',
+        referencia: "— Parábolas de Jesus, p. 85",
+        tags: "odio"
+    },
+    {
+        humor: "Ansiedade",
+        citacao: '"Muitos existem, porém que tornam duplamente pesados os fardos da vida por estarem continuamente antecipando aflições. Entretanto não precisa ser assim. Custará um decidido esforço mudar a corrente de seus pensamentos. Mas essa mudança pode acontecer."',
+        referencia: "— Como lidar com as emoções, p. 18",
+        tags: "nervosismo"
+    }
+
+]
