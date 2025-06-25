39fb7d403aa6c72d08a95d313da800e893a7a2b3
Julia Kuroishi <juliabkuroishi@gmail.com>
2025-06-25 10:39:01 -0400

    1 alteraçõa final

--- JavaScript/arrowfunction-numPar.js
+++ JavaScript/arrowfunction-numPar.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
+
 const meusNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const numerosFiltrados = meusNumeros
     .filter(numeros => numeros % 2 === 0);
 
-=======
-const meusNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const numerosFiltrados = meusNumeros
-    .filter(numeros => numeros % 2 === 0);
-
->>>>>>> e785069e8bd665914cebf066e1672c835b48b158
 console.log(numerosFiltrados);
